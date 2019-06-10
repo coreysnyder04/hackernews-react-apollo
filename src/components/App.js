c0b86ba211690c91f5c525dc094a1e4efca91f6c
Corey Snyder <corey.snyder@aver.io>
2019-06-10 17:01:12 -0400

    Adds ability to send GraphQL Mutation to add new links to the feed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList.jsx';
+import CreateLink from './CreateLink.jsx';
 
 function App() {
 
   return (
     <div className="App">
       <LinkList/>
+      <CreateLink/>
     </div>
   );
 }
