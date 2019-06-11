1dee33212c36641b54515990d6d314819977b175
Corey Snyder <corey.snyder@aver.io>
2019-06-10 21:24:46 -0400

    Implements react-router and basic header with Navigation

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList.jsx';
 import CreateLink from './CreateLink.jsx';
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="App">
-      <LinkList/>
-      <CreateLink/>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
     </div>
   );
 }
